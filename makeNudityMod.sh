2c181cea1537a0dfda0524539cecc4196ab7b2d6
Jason Rohrer <jasonrohrer@fastmail.fm>
2018-03-01 14:31:43 -0800

    Removed comment.

--- makeNudityMod.sh
+++ makeNudityMod.sh
@@ -15,7 +15,5 @@ cp sprites/600.tga nudityMod
 cd nudityMod
 
 for f in *.tga; do
-	#convert $f -alpha set -background none -channel A -evaluate multiply 0.50 +channel -channel R -evaluate set 1 +channel -channel G -evaluate set 255 -channel B -evaluate set 255  $f
-
 	convert $f -alpha set -background none -channel A -evaluate set 0% -channel R -evaluate set 100% -channel G -evaluate set 100% -channel B -evaluate set 100%  $f
 done
