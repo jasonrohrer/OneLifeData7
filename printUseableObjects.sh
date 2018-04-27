0b5233406a6c9e33744d7a5565b18b8b5ec071d4
Jason Rohrer <jasonrohrer@fastmail.fm>
2018-04-27 15:18:34 -0700

    Script prints use chance.

--- printUseableObjects.sh
+++ printUseableObjects.sh
@@ -13,10 +13,18 @@ if [[ $line == numUses* ]];
 then
 
 uses=$(echo $line | grep -o -E '[0-9]+' | head -1 );
+useChance=$(echo $line | sed 's/.*,//' | head -1 );
+
+
+if [[ $useChance == numUses* ]];
+then
+	useChance="1.0"
+fi
+
 
 if (( $(bc <<< "$uses > 1") ))
 then
-echo -e "$name\n  | uses = $uses" 
+echo -e "$name\n  | uses = $uses   [$useChance]" 
 fi
 
 break
