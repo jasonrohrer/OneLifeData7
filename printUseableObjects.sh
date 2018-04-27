55432ff471702308f4db64e5df8a4f6804fc7edc
Jason Rohrer <jasonrohrer@fastmail.fm>
2018-04-26 19:43:07 -0700

    Script for printing objects with numUses.

---
+++ printUseableObjects.sh
@@ -0,0 +1,30 @@
+for f in `ls -v objects/*`; do
+
+if [[ $f == objects/*.txt ]] && ! [[ $f == objects/nextObjectNumber.txt ]];
+then
+
+name=$(cat $f | sed -n 2p );
+
+while read -r line
+do
+
+
+if [[ $line == numUses* ]];
+then
+
+uses=$(echo $line | grep -o -E '[0-9]+' | head -1 );
+
+if (( $(bc <<< "$uses > 1") ))
+then
+echo -e "$name\n  | uses = $uses" 
+fi
+
+break
+fi
+
+    
+done < $f
+
+fi
+
+done
