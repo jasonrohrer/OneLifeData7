abfb96e4711020d636d33a330cdd1d5aa2d36663
Jason Rohrer <jasonrohrer@fastmail.fm>
2020-05-22 10:49:41 -0700

    Track carts now have 8 slots (was 4), making them more efficient.  Fixes #801

---
+++ addSlotsToCarts.sh
@@ -0,0 +1,11 @@
+for FILE in *; do grep -q Track $FILE && grep -q Cart $FILE && grep -q "numSlots=4" $FILE && echo $FILE; done > ../cartList.txt
+
+
+while read p; do
+	echo "$p"
+	
+	sed -i "s/numSlots=4/numSlots=8/" $p
+	
+	sed -i "s/numSprites=/slotPos=-18.000000,48.000000,vert=1,parent=-1\nslotPos=-6.000000,48.000000,vert=1,parent=-1\nslotPos=8.000000,48.000000,vert=1,parent=-1\nslotPos=17.000000,48.000000,vert=1,parent=\nnumSprites=/" $p
+
+done <../cartList.txt
